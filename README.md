f3669f9ff32025df5e232ade398196da9c4580dc
Dannii Willis <curiousdannii@gmail.com>
2020-08-13 16:55:09 +1000

    Little bit of a clean up Switch to Emscripten 2.0.0 Set Git definitions

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 Emglken: Glk meets Emscripten
 =============================
 
-This is an experimental project which provides a C Glk library for use with [Emscripten](http://kripken.github.io/emscripten-site/).
+Over the years many Interactive Fiction interpreters have been written which use the Glk API, or have been adapted to do so. Emglken takes some of these interpreters, compiles them to use the [RemGlk](https://github.com/erkyrath/remglk) Glk library using [Emscripten](https://emscripten.org/), and then outputs to Javascript and WebAssembly. These interpreters, which once needed to be compiled for each distinct operating system and CPU combination, can now be run anywhere there's a modern Javascript runtime: on the web with [Parchment](https://github.com/curiousdannii/parchment), in desktop apps like [Lectrote](https://github.com/erkyrath/lectrote), or in Node.js directly.
 
-Emglken is based on [Remglk](https://github.com/erkyrath/remglk) and is MIT licenced.
+Emglken itself doesn't have a lot of code, RemGlk does most of the work for us. What Emglken does provide is a virtual file system for Emscripten which lets RemGlk think it is running on a normal Linux filesystem, but is actually transformed to use [GlkOte](https://github.com/erkyrath/glkote)'s Dialog API. Emglken also provides a common interpreter interface to handle setting up the connections between each interpreter and GlkOte.
 
-Interpreters
-------------
+Both RemGlk and the Emglken customisations are MIT licensed, as are some of the interpreters, but others are licensed under other Free Software licenses as listed below.
 
-Name   | Upstream repo | Version | License
------- | ------------- | ------- | -------
-Git    | [DavidKinder/Git](https://github.com/DavidKinder/Git) | 1.3.5 | [MIT](https://github.com/DavidKinder/Git/blob/master/README.txt)
-Glulxe | [erkyrath/glulxe](https://github.com/erkyrath/glulxe) | 0.5.4 | [MIT](https://github.com/erkyrath/glulxe/blob/master/LICENSE)
-Hugo   | [curiousdannii/hugo](https://github.com/curiousdannii/hugo) | 3.3.0 | [BSD-2-Clause](https://github.com/curiousdannii/hugo/blob/master/LICENSE.txt)
+Included Projects
+-----------------
+
+Name   | Upstream repo | License
+------ | ------------- | -------
+Git    | [DavidKinder/Git](https://github.com/DavidKinder/Git) | [MIT](https://github.com/DavidKinder/Git/blob/master/README.txt)
+Glulxe | [erkyrath/glulxe](https://github.com/erkyrath/glulxe) | [MIT](https://github.com/erkyrath/glulxe/blob/master/LICENSE)
+Hugo   | [curiousdannii/hugo](https://github.com/curiousdannii/hugo) | [BSD-2-Clause](https://github.com/curiousdannii/hugo/blob/master/LICENSE.txt)
+RemGlk | [erkyrath/remglk](https://github.com/erkyrath/remglk) | [MIT](https://github.com/erkyrath/remglk/blob/master/LICENSE)
+TADS   | [cspiegel/terps](https://github.com/cspiegel/terps) | [GPL-2.0](https://github.com/cspiegel/terps/blob/master/tads/COPYING)
--- src/common.cmake
+++ src/common.cmake
@@ -10,68 +10,22 @@ endfunction()
 function(emglken_vm target)
     target_link_libraries(${target} remglk)
     target_link_options(${target} PRIVATE
-        -Wl,--wrap=getc,--wrap=ungetc
-        -O3
-        --minify 0
+        # Required options
         -sASYNCIFY=1
         -sASYNCIFY_IMPORTS=['emglken_fill_stdin_buffer']
-        #-sENVIRONMENT=web
         -sEXIT_RUNTIME=1
         #-sEXPORT_ES6=1
         -sEXPORTED_FUNCTIONS=['_main','_gidispatch_get_game_id']
         -sEXTRA_EXPORTED_RUNTIME_METHODS=['AsciiToString','FS']
         -sMODULARIZE=1
         -sSTRICT=1
+        -sWASM=1
+        -Wl,--wrap=getc,--wrap=ungetc
+        # Optimisations
+        --minify 0
+        -O3
+        # Building to just node and web saves about 1kb from the final JS
+        -sENVIRONMENT=node,web
         -sTEXTDECODER=2
-        -sWASM=1)
-    #emglken_whitelist(${target} ${VM_OPTS_EMTERPRETIFY_WHITELIST})
-endfunction()
-
-# Setup an EMTERPRETIFY whitelist file to be dependended on
-# From https://github.com/emscripten-core/emscripten/blob/master/cmake/Modules/Platform/Emscripten.cmake
-function(emglken_whitelist target jsfile)
-    # If the user edits the JS file, we want to relink the emscripten
-    # application, but unfortunately it is not possible to make a link step
-    # depend directly on a source file. Instead, we must make a dummy no-op
-    # build target on that source file, and make the project depend on
-    # that target.
-
-    # Sanitate the source .js filename to a good symbol name to use as a dummy
-    # filename.
-    get_filename_component(jsname "${jsfile}" NAME)
-    string(REGEX REPLACE "[/:\\\\.\ ]" "_" dummy_js_target ${jsname})
-    set(dummy_lib_name ${target}_${link_js_counter}_${dummy_js_target})
-    set(dummy_c_name "${CMAKE_BINARY_DIR}/${dummy_js_target}_tracker.c")
-
-    # Create a new static library target that with a single dummy .c file.
-    add_library(${dummy_lib_name} STATIC ${dummy_c_name})
-    # Make the dummy .c file depend on the .js file we are linking, so that if
-    # the .js file is edited, the dummy .c file, and hence the static library
-    # will be rebuild (no-op). This causes the main application to be
-    # relinked, which is what we want.  This approach was recommended by
-    # http://www.cmake.org/pipermail/cmake/2010-May/037206.html
-    add_custom_command(OUTPUT ${dummy_c_name} COMMAND ${CMAKE_COMMAND} -E touch ${dummy_c_name} DEPENDS ${jsfile})
-    target_link_libraries(${target} ${dummy_lib_name})
-
-    # Link the js-library to the target
-    # When a linked library starts with a "-" cmake will just add it to the
-    # linker command line as it is.  The advantage of doing it this way is
-    # that the js-library will also be automatically linked to targets that
-    # depend on this target.
-    get_filename_component(js_file_absolute_path "${jsfile}" ABSOLUTE )
-    target_link_options(${target} PRIVATE "SHELL:-s EMTERPRETIFY_WHITELIST=@${js_file_absolute_path}")
-endfunction()
-
-# Bundle VM
-function(make_bundle target core-target varname runner)
-    get_filename_component(runnerdest ${runner} NAME)
-    string(PREPEND runnerdest "runner-")
-    add_custom_command(OUTPUT ${target}
-        DEPENDS ${core-target} ${runner} ${CMAKE_SOURCE_DIR}/emglken/include/create_fake_stream.js ${CMAKE_SOURCE_DIR}/emglken/include/vm.js
-        COMMAND cp ${CMAKE_SOURCE_DIR}/${runner} ${CMAKE_CURRENT_BINARY_DIR}/${runnerdest}
-        COMMAND cp ${CMAKE_SOURCE_DIR}/emglken/include/create_fake_stream.js ${CMAKE_CURRENT_BINARY_DIR}/create_fake_stream.js
-        COMMAND cp ${CMAKE_SOURCE_DIR}/emglken/include/vm.js ${CMAKE_CURRENT_BINARY_DIR}/vm.js
-        COMMAND browserify ${CMAKE_CURRENT_BINARY_DIR}/${runnerdest} --bare --igv x --standalone ${varname} > ${CMAKE_CURRENT_BINARY_DIR}/${target}
-        COMMAND rm ${CMAKE_CURRENT_BINARY_DIR}/${runnerdest} ${CMAKE_CURRENT_BINARY_DIR}/create_fake_stream.js ${CMAKE_CURRENT_BINARY_DIR}/vm.js)
-    add_custom_target(${varname}-${target} ALL DEPENDS ${target})
+    )
 endfunction()
