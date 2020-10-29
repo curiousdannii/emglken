6bdd73819d87d268da1c1e3dc0cc8e49c7b472c2
Dannii Willis <curiousdannii@gmail.com>
2020-10-29 12:18:29 +1000

    Test TADS 2/3, closes #17

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,10 +2,14 @@
 
 cd "$(dirname "$0")"
 echo 'Git'
-python regtest.py -i "../bin/emglken.js --vm=git" glulxercise.regtest
+python regtest.py -i "../bin/emglken.js --vm=git" glulxercise.ulx.regtest
 echo 'Glulxe'
-python regtest.py -i "../bin/emglken.js --vm=glulxe" glulxercise.regtest
+python regtest.py -i "../bin/emglken.js --vm=glulxe" glulxercise.ulx.regtest
 #echo 'Glulxe profiler'
 #python regtest.py -i "../bin/emglken.js --profile_filename=glulxprof" glulxercise-profiler.regtest
 echo 'Hugo'
-python regtest.py -i "../bin/emglken.js" coretest.regtest
+python regtest.py -i "../bin/emglken.js" coretest.hex.regtest
+echo 'TADS 2'
+python regtest.py -i "../bin/emglken.js" ditch.gam.regtest
+echo 'TADS 3'
+python regtest.py -i "../bin/emglken.js" ditch3.t3.regtest
