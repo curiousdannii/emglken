e988bdd68d1c0d3e12374b381e83150f7ff8b9af
Dannii Willis <curiousdannii@gmail.com>
2022-09-22 11:41:06 +1000

    Test on all Node LTS releases in GH Actions

--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-#npm ci
-export PATH=$(npm bin):$PATH
+cd "$(dirname "$0")/.."
+
+EMVERSION=$(grep -oP '"emscripten-version":\s*"\K[\d.]+' package.json)
+
+echo "Building Emglken with Emscripten v$EMVERSION"
 
 mkdir -p build
-cd build
-emcmake cmake -DCMAKE_BUILD_TYPE=Release ..
-emmake make -j$(nproc)
+
+docker run --rm -t \
+    -u $(id -u):$(id -g) \
+    -v $(pwd):/src \
+    emscripten/emsdk:$EMVERSION \
+    /bin/bash -c " \\
+        emcmake cmake -DCMAKE_BUILD_TYPE=Release -S . -B build; \\
+        emmake make -j$(nproc) --no-print-directory -C build \\
+    "
