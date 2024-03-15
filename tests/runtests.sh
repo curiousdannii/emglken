fffd9ecc814dec25906451d098c5ff824411cac4
Dannii Willis <curiousdannii@gmail.com>
2024-03-15 13:48:38 +1000

    Use paths instead of SystemFileRefs. This means we're now committed to stop supporting the classic browser Dialog library Clean up some old source files

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -15,17 +15,16 @@ python regtest.py -i "../bin/emglken.js --rem=1 --vm=git" advent.ulx.regtest -t 
 echo 'Glulxe'
 python regtest.py -i "../bin/emglken.js --vm=glulxe" glulxercise.ulx.regtest -t 10
 python regtest.py -i "../bin/emglken.js --rem=1 --vm=glulxe" advent.ulx.regtest -t 10
-rm adventtest
 #echo 'Glulxe profiler'
 #python regtest.py -i "../bin/emglken.js --profile_filename=glulxprof" glulxercise-profiler.regtest
 echo 'Hugo'
 python regtest.py -i "../bin/emglken.js" coretest.hex.regtest -t 20
 python regtest.py -i "../bin/emglken.js --rem=1" colossal.hex.regtest -t 10
-rm hugotest
 echo 'Scare'
 python regtest.py -i "../bin/emglken.js --rem=1" Hamper.taf.regtest -t 10
-rm scaretest
 # echo 'TADS 2'
 # python regtest.py -i "../bin/emglken.js --rem=1" ditch.gam.regtest -t 20
 # echo 'TADS 3'
 # python regtest.py -i "../bin/emglken.js --rem=1" ditch3.t3.regtest -t 20
+
+rm *.glksave
