ba9b1f4528adfca87d02ec590ef306a07096e1f3
Dannii Willis <curiousdannii@gmail.com>
2024-12-11 23:08:31 +1000

    Change file-sizes.json to only have .wasm files, but to also have the gzipped size

--- tools/write-file-sizes.js
+++ tools/write-file-sizes.js
@@ -5,18 +5,26 @@
 import * as fs from 'fs'
 import {dirname, resolve} from 'path'
 import {fileURLToPath} from 'url'
+import {gzipSync} from 'zlib'
 
-const results = {
-    files: {},
-    files_gz: {},
-};
+const results = {};
+
+function add_file(name, path) {
+    const data = fs.readFileSync(path)
+    const data_gz = gzipSync(data)
+    results[name] = {
+        size: data.length,
+        gz: data_gz.length,
+    }
+}
 
 const build_path = resolve(dirname(fileURLToPath(import.meta.url)), '../build')
 for (const file of fs.readdirSync(build_path)) {
-    if (file.endsWith('.js') || file.endsWith('.wasm')) {
-        results.files[file] = fs.statSync(resolve(build_path, file)).size
-        // TODO: gzipped size
+    if (file.endsWith('.wasm')) {
+        add_file(file, resolve(build_path, file))
     }
 }
+
+add_file('glkaudio_bg.wasm', resolve(build_path, '../asyncglk/src/glkaudio/pkg/glkaudio_bg.wasm'))
 
 fs.writeFileSync(resolve(build_path, 'file-sizes.json'), JSON.stringify(results))
